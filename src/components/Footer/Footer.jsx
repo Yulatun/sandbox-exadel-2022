75ce6aeaa5befe5b6fd9b2226bcaa11d9f5a9215
unknown <egorshavluk98@gmail.com>
2022-07-13 22:38:01 +0200

    fix: remove all from footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,10 @@
-import { InfoOutlineIcon } from '@chakra-ui/icons';
-import { Center, Flex, Text } from '@chakra-ui/react';
-import i18next from 'i18next';
+import { Flex, Text } from '@chakra-ui/react';
 
 export const Footer = () => {
   return (
     <>
-      <Flex h="10vh" boxShadow="lg">
-        <Center flex="1 0 40%" align="center">
-          <Text fontSize="3xl">Space for something interesting</Text>
-        </Center>
-        <Flex flex="1 0 20%" justify="space-evenly" align="center">
-          <Text fontSize="5xl">Footer</Text>
-        </Flex>
-        <Flex flex="1 0 40%" direction="column" justify="center" align="center">
-          <Center>
-            <InfoOutlineIcon />
-            <Text m={(0, 1)}>{i18next.t('copyright')}</Text>
-          </Center>
-          <Text>{i18next.t('rights.reserved')}</Text>
-        </Flex>
+      <Flex h="10vh" boxShadow="lg" justify="center">
+        <Text fontSize="5xl">Footer</Text>
       </Flex>
     </>
   );
