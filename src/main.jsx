a45769a8fcae6ee55d6038d81a9db0e05c8c9705
Parkuel <taofdaydisu@gmail.com>
2022-07-13 14:06:42 +0300

    feat: added color mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { App } from './pages';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ColorModeScript } from './theme/colorModeButton';
+import theme from './theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
