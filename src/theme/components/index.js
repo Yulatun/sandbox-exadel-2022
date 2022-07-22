6abeeb5f461f01c685a19384e24be960e3f9a441
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-07-22 17:40:53 +0200

    feat: change default style for Switch

--- src/theme/components/index.js
+++ src/theme/components/index.js
@@ -2,3 +2,4 @@ export { Button } from './button';
 export { Form } from './form';
 export { Input } from './input';
 export { Modal } from './modal';
+export { Switch } from './switch';
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,6 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
 
-import { Button, Form, Input, Modal } from './components';
+import { Button, Form, Input, Modal, Switch } from './components';
 
 const config = {
   initialColorMode: 'system',
@@ -8,11 +8,18 @@ const config = {
 };
 
 export const theme = extendTheme({
+  colors: {
+    switch: {
+      200: '#38B2AC',
+      500: '#2C7A7B'
+    }
+  },
   components: {
     Button,
     Form,
     Input,
-    Modal
+    Modal,
+    Switch
   },
   config
 });
