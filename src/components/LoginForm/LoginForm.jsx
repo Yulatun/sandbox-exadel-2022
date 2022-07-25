78604a28c7d6531611fdfe24a246046ff1a42163
Yulatun <julialimbekov@gmail.com>
2022-07-25 18:18:54 +0300

    fix: remove hover from disabled button in Login Form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -132,6 +132,7 @@ export const LoginForm = () => {
             display="block"
             margin="0 auto"
             mt="20px"
+            _hover={isEmpty ? 'none' : null}
           >
             {i18next.t('login.form.btn.login')}
           </Button>
