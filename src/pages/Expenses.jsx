57306551f2e061cfb925865a181b9dbfb7eb3a7c
Parkuel <taofdaydisu@gmail.com>
2022-07-26 11:13:03 +0100

    feat: update expense colormode

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -1,13 +1,15 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 import i18next from 'i18next';
 
 import { Footer } from '@/components';
 
 export const Expenses = () => {
+  const cardBg = useColorModeValue('orange.50', 'teal.600');
+  const bgMain = useColorModeValue('orange.100', 'teal.900');
   return (
     <>
-      <Box bg="orange.300" w="100%" p={4} color="cornsilk">
-        <Flex direction="column" justify="center" align="center">
+      <Box bg={bgMain} w="100%" p={4}>
+        <Flex bg={cardBg} direction="column" justify="center" align="center">
           <main>
             <h2>{i18next.t('expenses.welcomeMessage')}</h2>
           </main>
