7b0afbb9036bf229b28ec0aa6fe0d34735bc43be
PavelTitov16 <pashoktitov16@gmail.com>
2022-07-29 20:07:38 +0300

    feat: add income component

--- src/pages/Incomes.jsx
+++ src/pages/Incomes.jsx
@@ -1,9 +1,50 @@
-import i18next from 'i18next';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Center,
+  Flex,
+  HStack,
+  IconButton,
+  Tooltip,
+  useColorModeValue
+} from '@chakra-ui/react';
 
-export const Incomes = () => {
+export const Incomes = ({ onEdit, onDelete }) => {
+  const bgMain = useColorModeValue('orange.50', 'teal.700');
+  const textColor = useColorModeValue('teal.900', 'orange.50');
+
   return (
-    <main>
-      <h2>{i18next.t('expenses.welcomeMessage')}</h2>
-    </main>
+    <>
+      <Center paddingY={3}>
+        <HStack
+          justify="space-evenly"
+          w="80%"
+          bg={bgMain}
+          color={textColor}
+          paddingY={3}
+          boxShadow="lg"
+        >
+          <Box w="8%">16.08.1993</Box>
+          <Box w="15%">
+            <Tooltip>Salary from Freelance</Tooltip>
+          </Box>
+          <Box w="10%">100$</Box>
+          <Box w="15%">
+            <Tooltip>Wallet in dollars</Tooltip>
+          </Box>
+
+          <Box w="10%">Notes</Box>
+
+          <Flex w="10%" justify="space-around">
+            <Box>
+              <IconButton onClick={onEdit} icon={<EditIcon />}></IconButton>
+            </Box>
+            <Box>
+              <IconButton onClick={onDelete} icon={<DeleteIcon />}></IconButton>
+            </Box>
+          </Flex>
+        </HStack>
+      </Center>
+    </>
   );
 };
