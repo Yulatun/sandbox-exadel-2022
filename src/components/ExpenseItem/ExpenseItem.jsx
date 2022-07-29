aaeb7c1e11a6d556b4c3024dba125d30a33624d5
Yulatun <julialimbekov@gmail.com>
2022-07-29 13:57:16 +0300

    fix: create short version of expenses

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -11,65 +11,95 @@ import {
 
 import { NoteIcon } from '@/assets';
 
-export const ExpenseItem = ({ transaction, onEdit, onDelete }) => {
+export const ExpenseItem = ({ transaction, onEdit, onDelete, version }) => {
   const bgMain = useColorModeValue('orange.50', 'teal.700');
   const textColor = useColorModeValue('teal.900', 'orange.50');
 
+  let fullVersionOfExpenseItem = (
+    <HStack
+      justify="space-around"
+      w="100%"
+      boxShadow="lg"
+      bg={bgMain}
+      color={textColor}
+      paddingY={3}
+    >
+      <Box w="8%">{transaction.date}</Box>
+      <Box
+        w="15%"
+        textOverflow="ellipsis"
+        overflow="hidden"
+        whiteSpace="nowrap"
+      >
+        <Tooltip label={transaction.category}>{transaction.category}</Tooltip>
+      </Box>
+      <Box w="7%">{transaction.amount}</Box>
+      <Box
+        w="20%"
+        textOverflow="ellipsis"
+        overflow="hidden"
+        whiteSpace="nowrap"
+      >
+        <Tooltip label={transaction.wallet}>{transaction.wallet}</Tooltip>
+      </Box>
+      <Box w="10%">{transaction.payer}</Box>
+      <Box
+        w="10%"
+        textOverflow="ellipsis"
+        overflow="hidden"
+        whiteSpace="nowrap"
+      >
+        <Tooltip label={transaction.notes}>
+          <Flex align="center" justify="center">
+            <NoteIcon color={textColor}></NoteIcon>
+          </Flex>
+        </Tooltip>
+      </Box>
+
+      <Flex w="10%" justify="space-around">
+        <Box>
+          <IconButton onClick={onEdit} icon={<EditIcon />}></IconButton>
+        </Box>
+        <Box>
+          <IconButton onClick={onDelete} icon={<DeleteIcon />}></IconButton>
+        </Box>
+      </Flex>
+    </HStack>
+  );
+
+  let shortVersionOfExpenseItem = (
+    <HStack
+      justify="space-around"
+      w="100%"
+      boxShadow="lg"
+      bg={bgMain}
+      color={textColor}
+      paddingY={3}
+    >
+      <Box w="8%">{transaction.date}</Box>
+      <Box
+        w="15%"
+        textOverflow="ellipsis"
+        overflow="hidden"
+        whiteSpace="nowrap"
+      >
+        <Tooltip label={transaction.category}>{transaction.category}</Tooltip>
+      </Box>
+      <Box w="7%">{transaction.amount}</Box>
+    </HStack>
+  );
+
+  const chooseVersion = (v) => {
+    if (v == 'short') {
+      return shortVersionOfExpenseItem;
+    } else {
+      return fullVersionOfExpenseItem;
+    }
+  };
+
   return (
     <>
-      <Center paddingY={3}>
-        <HStack
-          justify="space-around"
-          w="100%"
-          boxShadow="lg"
-          bg={bgMain}
-          color={textColor}
-          paddingY={3}
-        >
-          <Box w="8%">{transaction.date}</Box>
-          <Box
-            w="15%"
-            textOverflow="ellipsis"
-            overflow="hidden"
-            whiteSpace="nowrap"
-          >
-            <Tooltip label={transaction.category}>
-              {transaction.category}
-            </Tooltip>
-          </Box>
-          <Box w="7%">{transaction.amount}</Box>
-          <Box
-            w="20%"
-            textOverflow="ellipsis"
-            overflow="hidden"
-            whiteSpace="nowrap"
-          >
-            <Tooltip label={transaction.wallet}>{transaction.wallet}</Tooltip>
-          </Box>
-          <Box w="10%">{transaction.payer}</Box>
-          <Box
-            w="10%"
-            textOverflow="ellipsis"
-            overflow="hidden"
-            whiteSpace="nowrap"
-          >
-            <Tooltip label={transaction.notes}>
-              <Flex align="center" justify="center">
-                <NoteIcon color={textColor}></NoteIcon>
-              </Flex>
-            </Tooltip>
-          </Box>
-
-          <Flex w="10%" justify="space-around">
-            <Box>
-              <IconButton onClick={onEdit} icon={<EditIcon />}></IconButton>
-            </Box>
-            <Box>
-              <IconButton onClick={onDelete} icon={<DeleteIcon />}></IconButton>
-            </Box>
-          </Flex>
-        </HStack>
-      </Center>
+      <Center paddingY={3}>{chooseVersion(version)}</Center>
     </>
   );
 };
--- src/pages/WalletView.jsx
+++ src/pages/WalletView.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import { Box, Flex, useColorModeValue } from '@chakra-ui/react';
 
-import { Footer, WalletCard } from '@/components';
+import { ExpenseItem, Footer, WalletCard } from '@/components';
 
 export const WalletView = () => {
   const bgMain = useColorModeValue('orange.100', 'teal.900');
+  const transactionsTemplate = [
+    {
+      id: '2c984cb7-70c5-44e9-bc92-65661f3209d9',
+      date: '07.26',
+      category: 'food',
+      amount: 100,
+      wallet: 'name of Wallet',
+      payer: 'Me',
+      notes: 'Notes'
+    },
+    {
+      id: 'eb3608e5-4791-47a7-acfe-d3a7d9427e7f',
+      date: '07.25',
+      category: 'very long name of category',
+      amount: 200,
+      wallet: 'name of Wallet',
+      payer: 'Me',
+      notes: 'Notes'
+    },
+    {
+      id: '397cbf58-5558-4a3c-844d-f09650085b84',
+      date: '07.24',
+      category: 'go out',
+      amount: 300,
+      wallet: 'very long name of Wallet',
+      payer: 'Child',
+      notes: 'really super long-long notes about expenses'
+    }
+  ];
 
+  const transactions = transactionsTemplate.map((singleTransaction) => {
+    return (
+      <ExpenseItem
+        key={singleTransaction.id}
+        transaction={singleTransaction}
+        version="short"
+      />
+    );
+  });
   return (
     <>
       <Box bg={bgMain} h="700px" w="850">
@@ -16,6 +54,7 @@ export const WalletView = () => {
             name="nameOfWallet"
           ></WalletCard>
         </Flex>
+        {transactions}
       </Box>
       <Footer />
     </>
