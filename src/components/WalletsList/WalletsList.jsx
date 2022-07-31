2c211632395792229f4e2dac66371bde0197029c
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-07-31 19:28:31 +0200

    feat: use grid for responsiveness

--- src/components/WalletsList/WalletsList.jsx
+++ src/components/WalletsList/WalletsList.jsx
@@ -1,4 +1,12 @@
-import { Box, Flex, Text, useColorModeValue, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
+  useColorModeValue,
+  VStack
+} from '@chakra-ui/react';
 
 import { PiggyBankIcon } from '@/assets';
 
@@ -13,49 +21,65 @@ export const WalletsList = () => {
     <>
       <Box
         as="section"
-        w="100%"
+        w="full"
         maxW="container.xl"
         p={0}
         bg={bgSection}
         borderRadius={35}
         shadow="lg"
       >
-        <Flex justify="center">
-          <VStack
-            w="50vw"
-            p={8}
-            spacing={8}
-            alignItems="flex-start"
-            justify="center"
-            // bg="gray.400"
-          >
-            <Text as="h2" fontWeight="bold">
-              Total Balance:{' '}
-            </Text>
-            <Text>Choose your wallet</Text>
-            <AddWalletModal />
-          </VStack>
-          <VStack
-            w="100vw"
-            p={8}
-            spacing={8}
-            justify="center"
-            // bg="gray.300"
-          >
-            <WalletCard />
-          </VStack>
-          <VStack
-            w="50vw"
-            p={8}
-            spacing={8}
-            alignItems="center"
-            justify="center"
-            // bg="gray.400"
-          >
-            <PiggyBankIcon w={24} h={24} color={iconsThemeColor} />
-            <Text fontWeight="bold">0</Text>
-          </VStack>
-        </Flex>
+        <Grid
+          templateAreas={{
+            base: `'left right' 'center center'`,
+            md: `'left center right'`
+          }}
+          templateRows={'auto'}
+          templateColumns={{ base: '2fr 1fr', md: '1fr 3fr 1fr' }}
+        >
+          <GridItem area="left">
+            <VStack
+              w="full"
+              h="full"
+              p={8}
+              spacing={8}
+              alignItems="flex-start"
+              justify="center"
+              // bg="gray.400"
+            >
+              <Heading as="h4" size="md" fontWeight="bold">
+                Total Balance:{' '}
+              </Heading>
+              <Text>Choose your wallet</Text>
+              <AddWalletModal />
+            </VStack>
+          </GridItem>
+          <GridItem area="center">
+            <VStack
+              w="full"
+              h="full"
+              p={8}
+              spacing={8}
+              justify="center"
+              // bg="gray.300"
+            >
+              <WalletCard />
+            </VStack>
+          </GridItem>
+          <GridItem area="right">
+            <VStack
+              w="full"
+              h="full"
+              p={8}
+              spacing={8}
+              alignItems="center"
+              justify="center"
+              // bg="gray.200"
+            >
+              <PiggyBankIcon w={24} h={24} color={iconsThemeColor} />
+              <Text fontWeight="bold">0$</Text>
+            </VStack>
+          </GridItem>
+        </Grid>
       </Box>
     </>
   );
