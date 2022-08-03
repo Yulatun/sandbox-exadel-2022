2c229eb72dc06a65833caa92516f8e65667cb0be
Lizaveta <liz07bokan@gmail.com>
2022-08-03 21:29:55 +0200

    fix: fix wallewView page

--- src/pages/WalletView.jsx
+++ src/pages/WalletView.jsx
@@ -1,62 +1,89 @@
-import React from 'react';
-import { Box, Flex, useColorModeValue, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import {
+  Box,
+  Flex,
+  useColorModeValue,
+  useDisclosure,
+  VStack
+} from '@chakra-ui/react';
+import i18next from 'i18next';
 
-import { ExpenseItem, Footer, WalletCard } from '@/components';
+import { deleteTransactions, getTransactions } from '@/api/Transactions';
+import { DeleteConfirmationModal, ExpenseItem, WalletCard } from '@/components';
 
 export const WalletView = () => {
-  const bgMain = useColorModeValue('orange.100', 'teal.900');
-  const transactionsTemplate = [
+  const [chosenTransactionObj, setChosenTransactionObj] = useState({});
+
+  const deleteModal = useDisclosure();
+  const queryClient = useQueryClient();
+
+  const { data: dataTransactions, isFetched: isFetchedTransactions } = useQuery(
+    ['transactions'],
+    () => getTransactions()
+  );
+
+  const mutationTransaction = useMutation(
+    () => deleteTransactions(chosenTransactionObj.id),
     {
-      id: '2c984cb7-70c5-44e9-bc92-65661f3209d9',
-      date: '07.26',
-      category: 'food',
-      amount: 100,
-      wallet: 'name of Wallet',
-      payer: 'Me',
-      notes: 'Notes'
-    },
-    {
-      id: 'eb3608e5-4791-47a7-acfe-d3a7d9427e7f',
-      date: '07.25',
-      category: 'very long name of category',
-      amount: 200,
-      wallet: 'name of Wallet',
-      payer: 'Me',
-      notes: 'Notes'
-    },
-    {
-      id: '397cbf58-5558-4a3c-844d-f09650085b84',
-      date: '07.24',
-      category: 'go out',
-      amount: 300,
-      wallet: 'very long name of Wallet',
-      payer: 'Child',
-      notes: 'really super long-long notes about expenses'
+      onSuccess: () => {
+        queryClient.invalidateQueries(['transactions']);
+      }
     }
-  ];
+  );
+
+  const bgMain = useColorModeValue('orange.100', 'teal.900');
+
+  const onDelete = () => {
+    mutationTransaction.mutate();
+    deleteModal.onClose();
+  };
+  const onEdit = () => {
+    // code to edit
+  };
 
   return (
-    <>
-      <Box bg={bgMain} px={24} py={6} mt={6}>
-        <Flex mr="30%" ml="35%">
-          <WalletCard
-            totalBalance={2000}
-            currency="USD"
-            name="nameOfWallet"
-          ></WalletCard>
-        </Flex>
-        <VStack spacing={5} pt={5}>
-          {transactionsTemplate.map((singleTransaction) => (
-            <ExpenseItem
-              key={singleTransaction.id}
-              transaction={singleTransaction}
-              isShortView
-            />
-          ))}
-          ;
-        </VStack>
-      </Box>
-      <Footer />
-    </>
+    <Box bg={bgMain} px={24} py={6} mt={6}>
+      <Flex mr="30%" ml="35%">
+        <WalletCard
+          totalBalance={2000}
+          currency="USD"
+          name="nameOfWallet"
+        ></WalletCard>
+      </Flex>
+      <VStack spacing={5} pt={5}>
+        {isFetchedTransactions &&
+          dataTransactions.data
+            .filter((data) => data.id === data.id)
+            .map((dataTransaction) => (
+              <ExpenseItem
+                key={dataTransaction.id}
+                transaction={dataTransaction}
+                onEdit={onEdit}
+                onDelete={() => {
+                  setChosenTransactionObj(dataTransaction);
+                  deleteModal.onOpen();
+                }}
+              />
+            ))}
+      </VStack>
+      <DeleteConfirmationModal
+        isOpen={deleteModal.isOpen}
+        onSubmit={onDelete}
+        onClose={deleteModal.onClose}
+        title={
+          !!chosenTransactionObj.transactionType &&
+          chosenTransactionObj.transactionType === 'Income'
+            ? i18next.t('modal.deleteIncome.title')
+            : i18next.t('modal.deleteExpense.title')
+        }
+        text={
+          !!chosenTransactionObj.transactionType &&
+          chosenTransactionObj.transactionType === 'Income'
+            ? i18next.t('modal.deleteIncome.text')
+            : i18next.t('modal.deleteExpense.text')
+        }
+      />
+    </Box>
   );
 };
