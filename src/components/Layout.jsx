118691c99f2dac5a1db79299b9447d4be55497d3
HarryPotter-4000 <80061297+HarryPotter-4000@users.noreply.github.com>
2022-08-03 16:30:57 +0200

    style: add container to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import { Container } from '@chakra-ui/react';
 
 import { Header } from './Header';
 
@@ -6,8 +7,9 @@ export const AppLayout = () => {
   return (
     <>
       <Header />
-
-      <Outlet />
+      <Container maxWidth="container.xl">
+        <Outlet />
+      </Container>
     </>
   );
 };
