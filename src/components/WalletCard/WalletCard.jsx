e909f84d3db669e483645e4dfe407502b7da169d
alionakaliuzhnaya <alionakaliuzhnaya@icloud.com>
2022-08-03 21:24:35 +0200

    feat: add carousel for wallet

--- src/components/WalletCard/WalletCard.jsx
+++ src/components/WalletCard/WalletCard.jsx
@@ -27,6 +27,7 @@ export const WalletCard = (props) => {
   return (
     <>
       <Carousel
+        width="500px"
         showThumbs={false}
         showStatus={false}
         centerMode={true}
@@ -73,7 +74,141 @@ export const WalletCard = (props) => {
           )
         }
       >
-        <Box h="200px" m={5} mb={10} borderRadius={35} bg={bgMain} shadow="lg">
+        <Box
+          h="200px"
+          w="200px"
+          m={5}
+          mb={10}
+          borderRadius={35}
+          bg={bgMain}
+          shadow="lg"
+        >
+          <Badge
+            ml="80%"
+            mt="5%"
+            fontSize="50%"
+            align="center"
+            p={1}
+            bg={bgBadge}
+            borderRadius={5}
+            color={textColor}
+          >
+            {i18n.t('walletView.nameOfDefaultBadge')}
+          </Badge>
+
+          <Heading as="h1" size="md" color={textColor} ml={10}>
+            {props.name}
+          </Heading>
+
+          <Flex direction="column" m={5} align="center" color={textColor}>
+            <Flex>{i18n.t('walletView.headOfBalanceMessage')}</Flex>
+            <Flex fontSize="2xl" fontWeight="bold" color={textColor}>
+              {totalBalanceView}
+            </Flex>
+            <Text>{props.currency}</Text>
+          </Flex>
+          <Flex
+            direction="column"
+            p={2}
+            m={2}
+            align="start"
+            justify="flex-end"
+            h="20%"
+          ></Flex>
+        </Box>
+        <Box
+          h="200px"
+          w="200px"
+          m={5}
+          mb={10}
+          borderRadius={35}
+          bg={bgMain}
+          shadow="lg"
+        >
+          <Badge
+            ml="80%"
+            mt="5%"
+            fontSize="50%"
+            align="center"
+            p={1}
+            bg={bgBadge}
+            borderRadius={5}
+            color={textColor}
+          >
+            {i18n.t('walletView.nameOfDefaultBadge')}
+          </Badge>
+
+          <Heading as="h1" size="md" color={textColor} ml={10}>
+            {props.name}
+          </Heading>
+
+          <Flex direction="column" m={5} align="center" color={textColor}>
+            <Flex>{i18n.t('walletView.headOfBalanceMessage')}</Flex>
+            <Flex fontSize="2xl" fontWeight="bold" color={textColor}>
+              {totalBalanceView}
+            </Flex>
+            <Text>{props.currency}</Text>
+          </Flex>
+          <Flex
+            direction="column"
+            p={2}
+            m={2}
+            align="start"
+            justify="flex-end"
+            h="20%"
+          ></Flex>
+        </Box>
+        <Box
+          h="200px"
+          w="200px"
+          m={5}
+          mb={10}
+          borderRadius={35}
+          bg={bgMain}
+          shadow="lg"
+        >
+          <Badge
+            ml="80%"
+            mt="5%"
+            fontSize="50%"
+            align="center"
+            p={1}
+            bg={bgBadge}
+            borderRadius={5}
+            color={textColor}
+          >
+            {i18n.t('walletView.nameOfDefaultBadge')}
+          </Badge>
+
+          <Heading as="h1" size="md" color={textColor} ml={10}>
+            {props.name}
+          </Heading>
+
+          <Flex direction="column" m={5} align="center" color={textColor}>
+            <Flex>{i18n.t('walletView.headOfBalanceMessage')}</Flex>
+            <Flex fontSize="2xl" fontWeight="bold" color={textColor}>
+              {totalBalanceView}
+            </Flex>
+            <Text>{props.currency}</Text>
+          </Flex>
+          <Flex
+            direction="column"
+            p={2}
+            m={2}
+            align="start"
+            justify="flex-end"
+            h="20%"
+          ></Flex>
+        </Box>
+        <Box
+          h="200px"
+          w="200px"
+          m={5}
+          mb={10}
+          borderRadius={35}
+          bg={bgMain}
+          shadow="lg"
+        >
           <Badge
             ml="80%"
             mt="5%"
