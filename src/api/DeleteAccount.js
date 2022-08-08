fd557325a4af7517527644d7c20d7a70c403904d
Yulatun <julialimbekov@gmail.com>
2022-08-08 13:26:14 +0300

    fix: fix  request path

--- src/api/DeleteAccount.js
+++ src/api/DeleteAccount.js
@@ -2,5 +2,5 @@ import { instance } from './ApiProvider';
 
 export const deleteAccount = async () => {
   const id = 'c182e85e-af59-4793-ac75-e873a2d464e3';
-  return instance.delete(`/api/User/${id}`);
+  return instance.delete(`/api/v1/User/${id}`);
 };
