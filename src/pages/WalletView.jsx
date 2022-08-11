9db8b5fb42047bd4ff8554bebc2026e840930424
PavelTitov16 <pashoktitov16@gmail.com>
2022-08-11 10:34:18 +0300

    feat: add navigate to home page

--- src/pages/WalletView.jsx
+++ src/pages/WalletView.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { DeleteIcon } from '@chakra-ui/icons';
 import { Box, Flex, IconButton, useDisclosure, VStack } from '@chakra-ui/react';
 import i18next from 'i18next';
@@ -21,7 +22,6 @@ import {
   NotificationModal,
   WalletCard
 } from '@/components';
-import { PublicRoute } from '@/components/Routes/PublicRoute';
 import { useCentralTheme } from '@/theme';
 
 export const WalletView = () => {
@@ -31,7 +31,7 @@ export const WalletView = () => {
 
   const deleteWalletModal = useDisclosure();
   const deleteTransactionModal = useDisclosure();
-
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
 
   const { bgColor } = useCentralTheme();
@@ -59,6 +59,7 @@ export const WalletView = () => {
   const mutationWallet = useMutation(() => deleteWallet(walletId), {
     onSuccess: () => {
       queryClient.invalidateQueries(['wallets']);
+      navigate('/', { replace: true });
     }
   });
 
@@ -110,95 +111,93 @@ export const WalletView = () => {
   };
 
   return (
-    <PublicRoute redirectTo="/">
-      <Box bg={bgColor} px={24} py={6} mt={6}>
-        <IconButton
-          onClick={openWalletOnDelete}
-          ml="70%"
-          size="sm"
-          icon={<DeleteIcon />}
-        ></IconButton>
-        <Flex mr="30%" ml="35%">
-          {!!dataWallets && !!dataWallets.data && isFetchedWallets && (
-            <WalletCard
-              walletData={dataWallets.data.find(
-                (wallet) => wallet.id === walletId
-              )}
-            />
-          )}
-        </Flex>
-        <VStack spacing={5} pt={5}>
-          {!!dataIncomes &&
-            !!dataExpenses &&
-            !!dataIncomes.data &&
-            !!dataExpenses.data &&
-            isFetchedIncomes &&
-            isFetchedExpenses &&
-            [...dataIncomes.data, ...dataExpenses.data]
-              .filter((data) => data.walletId === walletId)
-              .sort((transactionA, transactionB) => {
-                transactionA.dateOfTransaction - transactionB.dateOfTransaction;
-              })
-              .map((dataTransaction) => {
-                return dataTransaction.transactionType === 'Income' ? (
-                  <IncomeItem
-                    key={dataTransaction.id}
-                    incomeData={dataTransaction}
-                    onEdit={onEdit}
-                    onDelete={() => {
-                      openTransactionOnDelete(dataTransaction);
-                    }}
-                  />
-                ) : (
-                  <ExpenseItem
-                    key={dataTransaction.id}
-                    expenseData={dataTransaction}
-                    onEdit={onEdit}
-                    onDelete={() => {
-                      openTransactionOnDelete(dataTransaction);
-                    }}
-                  />
-                );
-              })}
-        </VStack>
-        {!!dataUser &&
-        !!dataUser.data &&
-        isFetchedUser &&
-        dataUser.data.defaultWallet === walletId ? (
-          <NotificationModal
-            isOpen={deleteWalletModal.isOpen}
-            onSubmit={deleteWalletOnSubmit}
-            onClose={deleteWalletModal.onClose}
-            text={i18next.t('modal.deleteWalletDefault.text')}
-          />
-        ) : (
-          <DeleteConfirmationModal
-            isOpen={deleteWalletModal.isOpen}
-            onSubmit={deleteWalletOnSubmit}
-            onClose={deleteWalletModal.onClose}
-            title={i18next.t('modal.deleteWallet.title')}
-            text={i18next.t('modal.deleteWallet.text')}
+    <Box bg={bgColor} px={24} py={6} mt={6}>
+      <IconButton
+        onClick={openWalletOnDelete}
+        ml="70%"
+        size="sm"
+        icon={<DeleteIcon />}
+      ></IconButton>
+      <Flex mr="30%" ml="35%">
+        {!!dataWallets && !!dataWallets.data && isFetchedWallets && (
+          <WalletCard
+            walletData={dataWallets.data.find(
+              (wallet) => wallet.id === walletId
+            )}
           />
         )}
-
-        <DeleteConfirmationModal
-          isOpen={deleteTransactionModal.isOpen}
-          onSubmit={deleteTransactionOnSubmit}
-          onClose={deleteTransactionModal.onClose}
-          title={
-            !!chosenTransactionObj.transactionType &&
-            chosenTransactionObj.transactionType === 'Income'
-              ? i18next.t('modal.deleteIncome.title')
-              : i18next.t('modal.deleteExpense.title')
-          }
-          text={
-            !!chosenTransactionObj.transactionType &&
-            chosenTransactionObj.transactionType === 'Income'
-              ? i18next.t('modal.deleteIncome.text')
-              : i18next.t('modal.deleteExpense.text')
-          }
+      </Flex>
+      <VStack spacing={5} pt={5}>
+        {!!dataIncomes &&
+          !!dataExpenses &&
+          !!dataIncomes.data &&
+          !!dataExpenses.data &&
+          isFetchedIncomes &&
+          isFetchedExpenses &&
+          [...dataIncomes.data, ...dataExpenses.data]
+            .filter((data) => data.walletId === walletId)
+            .sort((transactionA, transactionB) => {
+              transactionA.dateOfTransaction - transactionB.dateOfTransaction;
+            })
+            .map((dataTransaction) => {
+              return dataTransaction.transactionType === 'Income' ? (
+                <IncomeItem
+                  key={dataTransaction.id}
+                  incomeData={dataTransaction}
+                  onEdit={onEdit}
+                  onDelete={() => {
+                    openTransactionOnDelete(dataTransaction);
+                  }}
+                />
+              ) : (
+                <ExpenseItem
+                  key={dataTransaction.id}
+                  expenseData={dataTransaction}
+                  onEdit={onEdit}
+                  onDelete={() => {
+                    openTransactionOnDelete(dataTransaction);
+                  }}
+                />
+              );
+            })}
+      </VStack>
+      {!!dataUser &&
+      !!dataUser.data &&
+      isFetchedUser &&
+      dataUser.data.defaultWallet === walletId ? (
+        <NotificationModal
+          isOpen={deleteWalletModal.isOpen}
+          onSubmit={deleteWalletOnSubmit}
+          onClose={deleteWalletModal.onClose}
+          text={i18next.t('modal.deleteWalletDefault.text')}
         />
-      </Box>
-    </PublicRoute>
+      ) : (
+        <DeleteConfirmationModal
+          isOpen={deleteWalletModal.isOpen}
+          onSubmit={deleteWalletOnSubmit}
+          onClose={deleteWalletModal.onClose}
+          title={i18next.t('modal.deleteWallet.title')}
+          text={i18next.t('modal.deleteWallet.text')}
+        />
+      )}
+
+      <DeleteConfirmationModal
+        isOpen={deleteTransactionModal.isOpen}
+        onSubmit={deleteTransactionOnSubmit}
+        onClose={deleteTransactionModal.onClose}
+        title={
+          !!chosenTransactionObj.transactionType &&
+          chosenTransactionObj.transactionType === 'Income'
+            ? i18next.t('modal.deleteIncome.title')
+            : i18next.t('modal.deleteExpense.title')
+        }
+        text={
+          !!chosenTransactionObj.transactionType &&
+          chosenTransactionObj.transactionType === 'Income'
+            ? i18next.t('modal.deleteIncome.text')
+            : i18next.t('modal.deleteExpense.text')
+        }
+      />
+    </Box>
   );
 };
