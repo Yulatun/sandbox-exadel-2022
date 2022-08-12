f2eb53fb1f41131f0ce70e0fe48a73d23c5d2c4f
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-08-12 18:21:51 +0200

    refactor: change logic & add default text colors

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,9 +1,10 @@
 import { useQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { Center, Heading } from '@chakra-ui/react';
 
 import { getUser } from '@/api/User';
 import { i18n } from '@/i18n';
+import { useCentralTheme } from '@/theme';
 
 export const Admin = () => {
   const { data: dataUser, isFetched: isFetchedUser } = useQuery(
@@ -11,24 +12,24 @@ export const Admin = () => {
     getUser
   );
 
-  const navigate = useNavigate();
+  const { textColor } = useCentralTheme();
 
   return (
     <>
       {!!dataUser &&
       !!dataUser.data &&
-      !!dataUser.data.isAdmin &&
+      !dataUser.data.isAdmin &&
       isFetchedUser ? (
+        <Navigate to="/" replace={true} />
+      ) : (
         <Center h="full" flexDirection="column">
-          <Heading as="h1" size="xl">
+          <Heading as="h1" color={textColor} size="xl">
             {i18n.t('admin.welcomePage')}
           </Heading>
-          <Heading as="h2" size="md" m={4}>
+          <Heading as="h2" color={textColor} size="md" m={4}>
             {i18n.t('admin.disclaimer')}
           </Heading>
         </Center>
-      ) : (
-        navigate('/', { replace: true })
       )}
     </>
   );
