f73e194912bb321d639fbdb15801170ba5fe9ef8
lazzzaroni <iamnotlazzaroni@gmail.com>
2022-08-16 12:58:20 +0200

    feat: create useAutoLogout hook

---
+++ src/hooks/useAutoLogout.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+
+import { logout } from '@/helpers/authorization';
+
+export const useAutoLogout = () => {
+  const logoutTimerIdRef = useRef(null);
+
+  useEffect(() => {
+    const autoLogout = () => {
+      if (document.visibilityState === 'hidden') {
+        const timeOutId = window.setTimeout(logout, 5 * 60 * 1000);
+        logoutTimerIdRef.current = timeOutId;
+      } else {
+        window.clearTimeout(logoutTimerIdRef.current);
+      }
+    };
+
+    document.addEventListener('visibilitychange', autoLogout);
+
+    return () => {
+      document.removeEventListener('visibilitychange', autoLogout);
+    };
+  }, []);
+};
